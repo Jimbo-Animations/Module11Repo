80ce2ce0ee2a067305c0d3d2d26a6a445d5498c5
Jimbo-Animations <73858605+Jimbo-Animations@users.noreply.github.com>
2025-07-31 10:44:19 -0400

    Update hello.py
    Added a second message.

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# James Barr
+# Firstname Lastname
 # 7/31/2025
 # A simple Python program to print a message
 
+# Firstname Lastname
+# 7/31/2025
+# Added new message
+
 print("Hello, World!")
+
+print("Welcome to Github!")
